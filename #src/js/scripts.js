a5b3aee16844d916cc1804aa9757ff57bd3a3c5a
yamirali <50670150+yamirali@users.noreply.github.com>
2021-05-29 00:22:01 +0600

    Запилил BEM
    Короч, закончил БЭМ, есть переход на страницу "Ивенты".
    Теперь иконки для меню добавляются не через css, а через js, то есть работает через array of objects. Вроде все...

--- #src/js/scripts.js
+++ #src/js/scripts.js
@@ -1,4 +1,5 @@
 @@include('testWebP.js')
+@@include('menu_icon.js')
 
 document.getElementById("re").addEventListener("click", () => {
     window.open("../../newsItemPage.html","_self")
@@ -6,8 +7,21 @@ document.getElementById("re").addEventListener("click", () => {
 var x = document.getElementById('dropdown-content');
 function myFunction() {
     if (x.style.display === 'none') {
-      x.style.display = 'block';
+        x.style.display = 'block';
     } else {
-      x.style.display = 'none';
+        x.style.display = 'none';
     }
-  }
+}
+
+for(var menuItem = 0; menuItem < menuIcons.length; menuItem++) {
+    var elem = document.getElementById(menuIcons[menuItem].id)
+    elem.style.backgroundImage = `url(${menuIcons[menuItem].idle})`
+    if(document.getElementById(menuIcons[menuItem].parentId).classList.contains("menu__item_active")) {
+        elem.style.backgroundImage = `url(${menuIcons[menuItem].active})`
+
+        // document.getElementById(menuIcons[menuItem].parentId).addEventListener("mouseenter",()=>{
+        //     elem.style.backgroundImage = `url(${menuIcons[menuItem].idle})`
+        // })
+    }
+    
+}
--- jastime/js/scripts.js
+++ jastime/js/scripts.js
@@ -16,6 +16,38 @@ function testWebP(callback) {
         }
     }
 );
+const menuIcons = [
+    {
+        parentId:   "menu__anchor-news",
+        id:         "menu__icon-news",
+        idle:       "../img/menu__icon-news.png",
+        active:     "../img/menu__icon-news-white.png"
+    },
+    {   
+        parentId:   "menu__anchor-events",
+        id:         "menu__icon-events",
+        idle:       "../img/menu__icon-events.png",
+        active:     "../img/menu__icon-events-white.png"
+    },
+    {
+        parentId:   "menu__anchor-compet",
+        id:         "menu__icon-compet",
+        idle:       "../img/menu__icon-compet.png",
+        active:     "../img/menu__icon-compet-white.png"
+    },
+    {   
+        parentId:   "menu__anchor-dvizh",
+        id:         "menu__icon-dvizh",
+        idle:       "../img/menu__icon-dvizh.png",
+        active:     "../img/menu__icon-dvizh-white.png"
+    },
+    {
+        parentId:   "menu__anchor-courses",
+        id:         "menu__icon-courses",
+        idle:       "../img/menu__icon-courses.svg",
+        active:     "../img/menu__icon-courses-white.png"
+    }
+]
 
 document.getElementById("re").addEventListener("click", () => {
     window.open("../../newsItemPage.html","_self")
@@ -23,8 +55,21 @@ document.getElementById("re").addEventListener("click", () => {
 var x = document.getElementById('dropdown-content');
 function myFunction() {
     if (x.style.display === 'none') {
-      x.style.display = 'block';
+        x.style.display = 'block';
     } else {
-      x.style.display = 'none';
+        x.style.display = 'none';
     }
-  }
+}
+
+for(var menuItem = 0; menuItem < menuIcons.length; menuItem++) {
+    var elem = document.getElementById(menuIcons[menuItem].id)
+    elem.style.backgroundImage = `url(${menuIcons[menuItem].idle})`
+    if(document.getElementById(menuIcons[menuItem].parentId).classList.contains("menu__item_active")) {
+        elem.style.backgroundImage = `url(${menuIcons[menuItem].active})`
+
+        // document.getElementById(menuIcons[menuItem].parentId).addEventListener("mouseenter",()=>{
+        //     elem.style.backgroundImage = `url(${menuIcons[menuItem].idle})`
+        // })
+    }
+    
+}
